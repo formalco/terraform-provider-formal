dfe4173555545830aee8ba7a283752d501481347
Brian Chan <brianchan2489@gmail.com>
2022-05-27 06:42:58 -0700

    enable integrating cloud accounts

---
+++ formal/resources/resource_cloud_account.go
@@ -0,0 +1,155 @@
+package resource
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/formalco/terraform-provider-formal/formal/api"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func ResourceCloudAccount() *schema.Resource {
+	return &schema.Resource{
+		// This description is used by the documentation generator and the language server.
+		Description: "Integrate a Cloud Account with Formal to deploy Managed Cloud resources. Note that the paired CloudFormation Stack must be deployed in eu-west-1 or us-east-1.",
+
+		CreateContext: resourceCloudAccountCreate,
+		ReadContext:   resourceCloudAccountRead,
+		UpdateContext: resourceCloudAccountUpdate,
+		DeleteContext: resourceCloudAccountDelete,
+
+		Schema: map[string]*schema.Schema{
+			"cloud_account_name": {
+				// This description is used by the documentation generator and the language server.
+				Description: "A friendly name to refer to this Cloud Account when using Formal.",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"cloud_provider": {
+				// This description is used by the documentation generator and the language server.
+				Description: "The Cloud Provider you are connecting the cloud account from. The only currently supported value is `aws`.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"aws_formal_stack_name": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the name field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"aws_formal_id": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the parameters.FormalID field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"aws_formal_iam_role": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the parameters.FormalIamRole field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"aws_formal_handshake_id": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the parameters.FormalHandshakeID field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"aws_formal_pingback_arn": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the parameters.FormalPingbackArn field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"aws_formal_template_body": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Use this value for the template_body field for your aws_cloudformation_stack resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func resourceCloudAccountCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*api.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// Maps to user-defined fields
+	cloudAccountName := d.Get("cloud_account_name").(string)
+	cloudProvider := d.Get("cloud_provider").(string)
+
+	if cloudProvider != "aws" {
+		return diag.FromErr(errors.New("cloud_provider must be 'aws'"))
+	}
+
+	createdAccount, err := client.CreateCloudAccount(cloudAccountName)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(createdAccount.Id)
+	tflog.Info(ctx, "id created is"+createdAccount.Id)
+	resourceCloudAccountRead(ctx, d, meta)
+	return diags
+}
+
+func resourceCloudAccountRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*api.Client)
+	var diags diag.Diagnostics
+
+	cloudAccount, err := client.GetCloudAccount(d.Id())
+	if err != nil {
+		if strings.Contains(fmt.Sprint(err), "status: 404") {
+			// CloudAccount was deleted
+			tflog.Warn(ctx, "The Cloud Account was not found, which means the stack was deleted or the integration was deleted without using this Terraform config,.", map[string]interface{}{"err": err})
+			d.SetId("")
+			return diags
+		}
+		return diag.FromErr(err)
+	}
+	if cloudAccount == nil {
+		tflog.Warn(ctx, "The Cloud Account was not found, which means it may have been deleted in the AWS console or otherwise without using this Terraform config.", map[string]interface{}{"err": err})
+		d.SetId("")
+		return diags
+	}
+
+	// Read AWS fields
+	d.Set("cloud_account_name", cloudAccount.CloudAccountName)
+	d.Set("cloud_provider", cloudAccount.CloudProvider)
+	d.Set("aws_formal_id", cloudAccount.AwsFormalId)
+	d.Set("aws_formal_iam_role", cloudAccount.AwsFormalIamRole)
+	d.Set("aws_formal_handshake_id", cloudAccount.AwsFormalHandshakeID)
+	d.Set("aws_formal_pingback_arn", cloudAccount.AwsFormalPingbackArn)
+	d.Set("aws_formal_stack_name", cloudAccount.AwsFormalStackName)
+	d.Set("aws_formal_template_body", cloudAccount.TemplateBody)
+
+	// d.Set("gcp_project_id", cloudAccount.GCPProjectID)
+
+	d.SetId(cloudAccount.Id)
+
+	return diags
+}
+
+func resourceCloudAccountUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return diag.Errorf("Group User Links are immutable. Please create a new roleLinkGroup.")
+}
+
+func resourceCloudAccountDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// Deletion is handled by a received webhoook when a CloudFormation stack is deleted
+	// Note that if a user create this resource, does not link it to CloudFormation, then tries to delete this resource, it wouldn't be removed from our DB.
+	// However this does not cause problems with terraform --
+	var diags diag.Diagnostics
+
+	tflog.Warn(ctx, "Please delete the CloudFormation stack to complete the delete process.")
+
+	d.SetId("")
+
+	return diags
+}
