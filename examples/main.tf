ac964ac85d89e074d2384ab6711ac7fdafcf2d1f
Brian Chan <brianchan2489@gmail.com>
2022-05-27 07:42:36 -0700

    notify about cleanup upon deleting cloud account integration

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>1.0.9"
+      version = "~>1.0.10"
     }
     aws = {
       source  = "hashicorp/aws"
