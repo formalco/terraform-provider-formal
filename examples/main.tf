df95ae2598231e5a73c8ae54286ac78ba89d516e
Brian Chan <brianchan2489@gmail.com>
2022-06-22 01:02:00 -0700

    enable updating sidecars with global_kms_decrypt

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>1.0.12"
+      version = "~>1.0.13"
     }
     aws = {
       source  = "hashicorp/aws"
