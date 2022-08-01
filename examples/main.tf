41255ba89a084e04292b39fc74d4e596721e50d0
Brian Chan <brianchan2489@gmail.com>
2022-08-01 06:40:42 -0700

    update version

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>1.0.21"
+      version = "~>1.0.22"
     }
     aws = {
       source  = "hashicorp/aws"
