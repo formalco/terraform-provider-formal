abf858c7b6e42eadcc956680c31fe43546a35dfc
Brian Chan <brianchan2489@gmail.com>
2022-08-02 05:03:12 -0700

    cloud_account_integration: handle deleted by CloudFormation

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>1.0.24"
+      version = "~>1.0.25"
     }
     aws = {
       source  = "hashicorp/aws"
