ce1dcaa48a5ea77cde9988a2f682a380ad37e15d
Mokhtar Bacha <mokhtar@joinformal.com>
2022-09-19 19:12:22 +0200

    add documentation about blocking policies

---
+++ examples/policies/variables.tf
@@ -0,0 +1,13 @@
+variable "formal_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "formal_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "group_id" {
+  type = string
+}
