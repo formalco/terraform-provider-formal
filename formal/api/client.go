a3d3cfdc8c221e94f4532c782a497c498b8b6bb8
Mokhtar Bacha <mokhtar@joinformal.com>
2022-09-25 15:34:37 +0200

    replace io/ioutil package by io

--- formal/api/client.go
+++ formal/api/client.go
@@ -3,7 +3,7 @@ package api
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 )
@@ -50,7 +50,7 @@ func (c *Client) doRequest(req *http.Request) ([]byte, error) {
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
