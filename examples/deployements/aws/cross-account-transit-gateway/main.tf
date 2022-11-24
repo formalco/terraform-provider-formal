cff8f580d3aaea049c71365f7439828609c8e519
Brian Chan <brianchan2489@gmail.com>
2022-11-24 21:24:05 +0000

    bump to v1.0.54

--- examples/deployements/aws/cross-account-transit-gateway/main.tf
+++ examples/deployements/aws/cross-account-transit-gateway/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>1.0.53"
+      version = "~>1.0.54"
     }
     aws = {
       source  = "hashicorp/aws"
