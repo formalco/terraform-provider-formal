71804a28c3ce09067d69c6f23467f1eb5d43a8ca
Brian Chan <brianchan2489@gmail.com>
2022-11-29 13:43:50 -0500

    getDatastoreForStatus CreatedAt change from string to int

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>2.0.3"
+      version = "~>2.0.4"
     }
     aws = {
       source  = "hashicorp/aws"
