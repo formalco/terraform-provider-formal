d82f20178861bcbfcd4eb38d41677d4beb37351d
Brian Chan <brianchan2489@gmail.com>
2022-11-29 11:57:00 -0500

    enable update default_access_behavior

--- examples/deployements/aws/cross-account-transit-gateway/main.tf
+++ examples/deployements/aws/cross-account-transit-gateway/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~> 2.0.2"
+      version = "~> 2.0.3"
     }
     aws = {
       source  = "hashicorp/aws"
