be41da7944b4ebec42d5e61e2524fb678cc89e6b
Mokhtar Bacha <mokhtar@joinformal.com>
2023-03-27 22:25:18 +0200

    update doc

--- examples/deployements/aws/no-vpc/main.tf
+++ examples/deployements/aws/no-vpc/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>3.0.12"
+      version = "~>3.0.13"
     }
     aws = {
       source  = "hashicorp/aws"
