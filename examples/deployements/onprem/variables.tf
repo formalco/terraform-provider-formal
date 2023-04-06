a2bd9a7c46616a0c1512b4ddda43e23b60e0f952
Mokhtar Bacha <mokhtar@joinformal.com>
2023-04-06 11:17:51 -0400

    add example for onprem sidecar

---
+++ examples/deployements/onprem/variables.tf
@@ -0,0 +1,14 @@
+variable "formal_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "name" {}
+
+
+variable "snowflake_username" {}
+variable "snowflake_password" {}
+variable "snowflake_hostname" {}
+variable "snowflake_port" {}
+
+variable "sidecar_hostname" {}
