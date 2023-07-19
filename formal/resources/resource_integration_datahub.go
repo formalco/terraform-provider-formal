a95c162de9333f538dcd4d45a7549c9687ebb9f5
VladyslavLukyanenko <me@vladyslav.xyz>
2023-07-19 18:42:06 +0200

    datahub

--- formal/resources/resource_integration_datahub.go
+++ formal/resources/resource_integration_datahub.go
@@ -16,6 +16,7 @@ func ResourceIntegrationDatahub() *schema.Resource {
 		Description:   "Registering a Datahub integration.",
 		CreateContext: resourceIntegrationDatahubCreate,
 		ReadContext:   resourceIntegrationDatahubRead,
+		UpdateContext: resourceIntegrationDatahubUpdate,
 		DeleteContext: resourceIntegrationDatahubDelete,
 
 		Timeouts: &schema.ResourceTimeout{
@@ -30,12 +31,6 @@ func ResourceIntegrationDatahub() *schema.Resource {
 				Description: "The ID of the Integration.",
 				Type:        schema.TypeString,
 				Computed:    true,
-			},
-			"name": {
-				// This description is used by the documentation generator and the language server.
-				Description: "Friendly name for the Integration.",
-				Type:        schema.TypeString,
-				Required:    true,
 			},
 			"webhook_secret": {
 				// This description is used by the documentation generator and the language server.
@@ -55,6 +50,27 @@ func ResourceIntegrationDatahub() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 			},
+			"active": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Active status of the Integration.",
+				Type:        schema.TypeBool,
+				Required:    true,
+			},
+			"api_key": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Api Key for the GMS server.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"subscribed_entities": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Subscribed entities of the Integration: currently supported values are 'tags', 'data_labels'.",
+				Type:        schema.TypeList,
+				Required:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
 			"organization_id": {
 				// This description is used by the documentation generator and the language server.
 				Description: "Organization ID of the Integration.",
@@ -70,13 +86,22 @@ func resourceIntegrationDatahubCreate(ctx context.Context, d *schema.ResourceDat
 
 	var diags diag.Diagnostics
 
-	Name := d.Get("name").(string)
+	var subscribedEntities []string
+	for _, subscribedEntity := range d.Get("subscribed_entities").([]interface{}) {
+		subscribedEntities = append(subscribedEntities, subscribedEntity.(string))
+	}
+
 	CommunicationType := d.Get("communication_type").(string)
 	GMSUrl := d.Get("gms_url").(string)
+	Active := d.Get("active").(bool)
+	ApiKey := d.Get("api_key").(string)
+
 	res, err := c.Grpc.Sdk.DatahubServiceClient.CreateDatahubIntegration(ctx, connect.NewRequest(&adminv1.CreateDatahubIntegrationRequest{
-		Name:              Name,
-		CommunicationType: CommunicationType,
-		GsmUrl:            GMSUrl,
+		CommunicationType:  CommunicationType,
+		GmsUrl:             GMSUrl,
+		SubscribedEntities: subscribedEntities,
+		Active:             Active,
+		ApiKey:             ApiKey,
 	}))
 	if err != nil {
 		return diag.FromErr(err)
@@ -100,29 +125,65 @@ func resourceIntegrationDatahubRead(ctx context.Context, d *schema.ResourceData,
 		return diag.FromErr(err)
 	}
 
-	//d.Set("name", res.Msg.Integration.Name)
-	//d.Set("communication_type", res.Msg.Integration.CommunicationType)
-	//d.Set("gms_url", res.Msg.Integration.)
+	if res.Msg.Integration == nil {
+		d.SetId("")
+		return diags
+	}
+
+	d.Set("communication_type", res.Msg.Integration.CommunicationType)
+	d.Set("gms_url", res.Msg.Integration.GmsUrl)
 	d.Set("webhook_secret", res.Msg.Integration.WebhookSecret)
 	d.Set("organization_id", res.Msg.Integration.OrganizationId)
+	d.Set("active", res.Msg.Integration.Active)
+	d.Set("subscribed_entities", res.Msg.Integration.SubscribedEntities)
 
 	d.SetId(res.Msg.Integration.Id)
 	return diags
 }
 
-func resourceIntegrationDatahubDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	//c := meta.(*clients.Clients)
+func resourceIntegrationDatahubUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*clients.Clients)
 	var diags diag.Diagnostics
 
-	//appId := d.Id()
-	//
-	//_, err := c.Grpc.Sdk.DatahubServiceClient.DeleteIntegrationApp(ctx, connect.NewRequest(&adminv1.DeleteIntegrationAppRequest{Id: appId}))
-	//
-	//if err != nil {
-	//	return diag.FromErr(err)
-	//}
-	//
-	//d.SetId("")
+	var subscribedEntities []string
+	for _, subscribedEntity := range d.Get("subscribed_entities").([]interface{}) {
+		subscribedEntities = append(subscribedEntities, subscribedEntity.(string))
+	}
+
+	CommunicationType := d.Get("communication_type").(string)
+	GMSUrl := d.Get("gms_url").(string)
+	Active := d.Get("active").(bool)
+	ApiKey := d.Get("api_key").(string)
+
+	_, err := c.Grpc.Sdk.DatahubServiceClient.UpdateDatahubIntegration(ctx, connect.NewRequest(&adminv1.UpdateDatahubIntegrationRequest{
+		Id:                 d.Id(),
+		CommunicationType:  CommunicationType,
+		GmsUrl:             GMSUrl,
+		SubscribedEntities: subscribedEntities,
+		Active:             Active,
+		ApiKey:             ApiKey,
+	}))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}
+
+func resourceIntegrationDatahubDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*clients.Clients)
+	var diags diag.Diagnostics
+
+	appId := d.Id()
+
+	_, err := c.Grpc.Sdk.DatahubServiceClient.DeleteDatahubIntegration(ctx, connect.NewRequest(&adminv1.DeleteDatahubIntegrationRequest{Id: appId}))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
 
 	return diags
 }
