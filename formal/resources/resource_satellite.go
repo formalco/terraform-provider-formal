adc0d443725a602efe563776d48cfa13a2e8dcd0
VladyslavLukyanenko <me@vladyslav.xyz>
2023-08-02 11:23:41 +0200

    add satellite resource

--- formal/resources/resource_satellite.go
+++ formal/resources/resource_satellite.go
@@ -3,10 +3,14 @@ package resource
 import (
 	adminv1 "buf.build/gen/go/formal/admin/protocolbuffers/go/admin/v1"
 	"context"
+	"errors"
+	"fmt"
 	"github.com/bufbuild/connect-go"
 	"github.com/formalco/terraform-provider-formal/formal/clients"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"strconv"
 	"time"
 )
 
@@ -60,6 +64,36 @@ func resourceSatelliteCreate(ctx context.Context, d *schema.ResourceData, meta i
 	}))
 	if err != nil {
 		return diag.FromErr(err)
+	}
+
+	const ErrorTolerance = 5
+	currentErrors := 0
+	for {
+		// Retrieve status
+		createdSatellite, err := c.Grpc.Sdk.SatelliteServiceClient.GetSatelliteById(ctx, connect.NewRequest(&adminv1.GetSatelliteByIdRequest{Id: res.Msg.Id}))
+		if err != nil {
+			if currentErrors >= ErrorTolerance {
+				return diag.FromErr(err)
+			} else {
+				tflog.Warn(ctx, "Experienced an error #"+strconv.Itoa(currentErrors+1)+" retrieving Satellite: ", map[string]interface{}{"err": err})
+				currentErrors += 1
+				time.Sleep(15 * time.Second)
+				continue
+			}
+		}
+
+		if createdSatellite.Msg.Satellite == nil {
+			err = errors.New("satellite with the given ID not found. It may have been deleted")
+			return diag.FromErr(err)
+		}
+
+		tflog.Info(ctx, "Satellite status is: "+fmt.Sprint(createdSatellite.Msg.Satellite.Status))
+		// Check status
+		if createdSatellite.Msg.Satellite.Status == "ready" {
+			break
+		} else {
+			time.Sleep(15 * time.Second)
+		}
 	}
 
 	d.SetId(res.Msg.Id)
