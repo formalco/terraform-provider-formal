215059a53860b646f787d9a791f776ae1add6913
Mokhtar Bacha <mokhtar@joinformal.com>
2023-09-01 19:21:32 +0700

    add support for examples

--- examples/deployements/aws/onprem/ssh/nlb.tf
+++ examples/deployements/aws/onprem/ssh/nlb.tf
@@ -41,7 +41,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 # Redirect traffic to target group
-resource "aws_lb_listener" "grpc" {
+resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.id
   port              = var.main_port
   protocol          = "TCP"
