b459a7128f8166cef6001fc99f99194c374e7520
Loris Wintjens <loris.wintjens@gmail.com>
2023-09-23 05:40:14 +0700

    add redshift onprem

--- examples/deployements/aws/onprem/redshift/redshift_proxy/formal.tf
+++ examples/deployements/aws/onprem/redshift/redshift_proxy/formal.tf
@@ -18,11 +18,11 @@ resource "formal_sidecar" "main" {
   deployment_type    = "onprem"
   technology         = "redshift"
   global_kms_decrypt = false
-  network_type       = "internal"
+  network_type       = "internet-facing"
   formal_hostname    = var.redshift_sidecar_hostname
 }
 
-resource "formal_datastore" "main" {
+resource "formal_datastore" "ds" {
   technology = "redshift"
   name       = var.name
   hostname   = var.redshift_hostname
@@ -30,14 +30,14 @@ resource "formal_datastore" "main" {
 }
 
 resource "formal_sidecar_datastore_link" "main" {
-  datastore_id = formal_datastore.main.id
+  datastore_id = formal_datastore.ds.id
   sidecar_id   = formal_sidecar.main.id
-  port         = 443
+  port         = 5439
 }
 
 # Native Role
 resource "formal_native_role" "main_redshift" {
-  datastore_id       = formal_datastore.main.id
+  datastore_id       = formal_datastore.ds.id
   native_role_id     = var.redshift_username
   native_role_secret = var.redshift_password
   use_as_default     = true // per sidecar, exactly one native role must be marked as the default.
