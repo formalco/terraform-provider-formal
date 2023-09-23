6f3d91fc3f1cf5e7bcbdd747231ee99227fc27de
Loris Wintjens <loris.wintjens@gmail.com>
2023-09-23 15:36:05 +0700

    all in one

---
+++ examples/deployements/aws/onprem/all-in-one/variables.tf
@@ -0,0 +1,75 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "formal_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "name" {}
+variable "environment" {}
+
+variable "cidr" {
+  default = "172.0.0.0/16"
+}
+variable "private_subnets" {
+  default = ["172.0.0.0/20", "172.0.32.0/20", "172.0.64.0/20"]
+}
+variable "public_subnets" {
+  default = ["172.0.16.0/20", "172.0.48.0/20", "172.0.80.0/20"]
+}
+variable "availability_zones" {
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "datadog_api_key" {}
+
+variable "dockerhub_username" {}
+variable "dockerhub_password" {}
+
+variable "container_cpu" {
+  default = 2048
+}
+variable "container_memory" {
+  default = 4096
+}
+
+variable "health_check_port" {
+  default = 8080
+}
+variable "snowflake_port" {
+  default = 443
+}
+variable "data_classifier_satellite_port" {
+  default = 50055
+}
+
+variable "snowflake_container_image" {}
+variable "data_classifier_satellite_container_image" {}
+
+
+variable "snowflake_sidecar_hostname" {}
+variable "snowflake_hostname" {}
+
+variable "snowflake_username" {}
+variable "snowflake_password" {}
+
+variable "http_port" {
+  default = 443
+}
+
+variable "postgres_port" {
+  default = 5432
+}
+
+variable "postgres_container_image" {}
+variable "postgres_sidecar_hostname" {}
+
+variable "postgres_username" {}
+variable "postgres_password" {}
+
+variable "http_container_image" {}
+
+variable "http_sidecar_hostname" {}
+variable "http_hostname" {}
