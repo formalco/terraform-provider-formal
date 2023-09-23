74f923162bb51b4b9a175a370331058429e0c378
Loris Wintjens <loris.wintjens@gmail.com>
2023-09-23 23:53:45 +0700

    kms

--- examples/deployements/aws/onprem/s3/environment.tfvars
+++ examples/deployements/aws/onprem/s3/environment.tfvars
@@ -3,8 +3,8 @@ environment = ""
 
 formal_api_key = ""
 
-region             = "us-west-2"
-availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+region             = "ap-southeast-2"
+availability_zones = ["ap-southeast-2a", "ap-southeast-2c", "ap-southeast-2b"]
 private_subnets    = ["172.0.0.0/20", "172.0.32.0/20", "172.0.64.0/20"]
 public_subnets     = ["172.0.16.0/20", "172.0.48.0/20", "172.0.80.0/20"]
 
--- examples/deployements/aws/onprem/s3/s3_proxy/s3.tf
+++ examples/deployements/aws/onprem/s3/s3_proxy/s3.tf
@@ -41,32 +41,6 @@ resource "aws_iam_access_key" "example_access_key" {
   user = aws_iam_user.aws_native_user.name
 }
 
-resource "aws_s3_bucket_cors_configuration" "b" {
-  bucket = aws_s3_bucket.b.id  
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }  
-}
-
-resource "aws_s3_bucket_acl" "b" {
-    bucket = aws_s3_bucket.b.id
-    acl    = "public-read"
-    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
-}
-
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.b.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.bucket]
-}
-
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = aws_s3_bucket.b.id
@@ -75,40 +49,6 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "bucket" {
-    bucket = aws_s3_bucket.b.id
-    policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Principal = "*"
-        Action = [
-          "s3:*",
-        ]
-        Effect = "Allow"
-        Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
-        ]
-      },
-      {
-        Sid = "PublicReadGetObject"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-        ]
-        Effect   = "Allow"
-        Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
-        ]
-      },
-    ]
-  })
-  
-  depends_on = [aws_s3_bucket_public_access_block.bucket]
 }
 
 output "iam_access_key_id" {
