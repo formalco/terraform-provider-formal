ac0ee706ca0629873a13787566d056bbdfecceec
Loris Wintjens <loris.wintjens@gmail.com>
2023-09-23 14:20:02 +0700

    add s3 onprem

---
+++ examples/deployements/aws/onprem/s3/data_classifier_satellite/secrets.tf
@@ -0,0 +1,8 @@
+resource "aws_secretsmanager_secret" "formal_tls_cert" {
+  name = "${var.name}-formal-tls-cert-2"
+}
+
+resource "aws_secretsmanager_secret_version" "formal_tls_cert" {
+  secret_id     = aws_secretsmanager_secret.formal_tls_cert.id
+  secret_string = formal_satellite.main.tls_cert
+}
---
+++ examples/deployements/aws/onprem/s3/variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  default = "ap-southeast-2"
+}
+
+variable "formal_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "name" {}
+variable "environment" {}
+
+variable "cidr" {
+  default = "172.0.0.0/16"
+}
+variable "private_subnets" {
+  default = ["172.0.0.0/20", "172.0.32.0/20", "172.0.64.0/20"]
+}
+variable "public_subnets" {
+  default = ["172.0.16.0/20", "172.0.48.0/20", "172.0.80.0/20"]
+}
+variable "availability_zones" {
+  default = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+}
+
+variable "datadog_api_key" {}
+
+variable "dockerhub_username" {}
+variable "dockerhub_password" {}
+
+variable "container_cpu" {
+  default = 2048
+}
+variable "container_memory" {
+  default = 4096
+}
+
+variable "health_check_port" {
+  default = 8080
+}
+variable "s3_port" {
+  default = 443
+}
+variable "data_classifier_satellite_port" {
+  default = 50055
+}
+
+variable "s3_container_image" {}
+variable "data_classifier_satellite_container_image" {}
+
+
+variable "s3_sidecar_hostname" {}
