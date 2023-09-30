d841c0a7f6664fa26c2f8eddc3cfa80cfa4f3e03
Loris Wintjens <loris.wintjens@gmail.com>
2023-09-30 14:26:18 +0200

    update: add helm_release example

--- examples/deployements/aws/onprem/helm-eks-http-sidecar/terraform/helm/variables.tf
+++ examples/deployements/aws/onprem/helm-eks-http-sidecar/terraform/helm/variables.tf
@@ -2,7 +2,5 @@ variable "eks_cluster_name" {}
 variable "eks_cluster_endpoint" {}
 variable "eks_cluster_certificate_authority_data" {}
 
-variable "aws_ecr_pwd" {}
 
-variable "ecr_repository_oci_url" {}
 variable "chart_oci" {}
