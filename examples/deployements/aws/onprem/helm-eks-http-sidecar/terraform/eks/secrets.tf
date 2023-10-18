1e60ff0e863d161472caffc9cc1d6bc1831d7cbf
Loris Wintjens <loris.wintjens@gmail.com>
2023-10-18 22:45:18 +0200

    upd

--- examples/deployements/aws/onprem/helm-eks-http-sidecar/terraform/eks/secrets.tf
+++ examples/deployements/aws/onprem/helm-eks-http-sidecar/terraform/eks/secrets.tf
@@ -4,7 +4,7 @@ resource "aws_secretsmanager_secret" "formal_tls_cert" {
 
 resource "aws_secretsmanager_secret_version" "formal_tls_cert" {
   secret_id     = aws_secretsmanager_secret.formal_tls_cert.id
-  secret_string = var.formal_sidecar_cert
+  secret_string = 
 }
 
 resource "aws_secretsmanager_secret" "formal_data_classifier_tls_cert" {
@@ -13,5 +13,5 @@ resource "aws_secretsmanager_secret" "formal_data_classifier_tls_cert" {
 
 resource "aws_secretsmanager_secret_version" "formal_data_classifier_tls_cert" {
   secret_id     = aws_secretsmanager_secret.formal_data_classifier_tls_cert.id
-  secret_string = var.formal_data_classifier_cert
+  secret_string = 
 }
