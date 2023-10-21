4965f007acc63f238e95aeabdb0ecc6fb9438a42
VladyslavLukyanenko <me@vladyslav.xyz>
2023-10-21 23:20:20 +0200

    use id instead

--- formal/resources/resource_datastore.go
+++ formal/resources/resource_datastore.go
@@ -132,7 +132,6 @@ func resourceDatastoreCreate(ctx context.Context, d *schema.ResourceData, meta i
 		return diag.FromErr(err)
 	}
 
-	// DsId is the UUID type id. See GetDatastoreInfraByDatastoreID in admin-api for more details
 	d.SetId(res.Msg.Id)
 
 	resourceDatastoreRead(ctx, d, meta)
@@ -161,7 +160,7 @@ func resourceDatastoreRead(ctx context.Context, d *schema.ResourceData, meta int
 		return diag.FromErr(err)
 	}
 
-	d.Set("id", res.Msg.Datastore.DatastoreId)
+	d.Set("id", res.Msg.Datastore.Id)
 	d.Set("name", res.Msg.Datastore.Name)
 	d.Set("hostname", res.Msg.Datastore.Hostname)
 	d.Set("port", res.Msg.Datastore.Port)
@@ -169,8 +168,7 @@ func resourceDatastoreRead(ctx context.Context, d *schema.ResourceData, meta int
 	d.Set("created_at", res.Msg.Datastore.CreatedAt.AsTime().Unix())
 	d.Set("environment", res.Msg.Datastore.Environment)
 
-	// DsId is the UUID type id. See GetDatastoreInfraByDatastoreID in admin-api for more details
-	d.SetId(res.Msg.Datastore.DatastoreId)
+	d.SetId(res.Msg.Datastore.Id)
 
 	return diags
 }
