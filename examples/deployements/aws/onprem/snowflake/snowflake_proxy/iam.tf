a23819bc8631de6e63bf0d1145fc6aa6962e5946
Loris Wintjens <loris.wintjens@gmail.com>
2023-11-04 01:14:25 +0100

    snow

--- examples/deployements/aws/onprem/snowflake/snowflake_proxy/iam.tf
+++ examples/deployements/aws/onprem/snowflake/snowflake_proxy/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_execution_role"
+  name = "ecs_execution_role_snowflake"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment"
 }
 
 resource "aws_iam_policy" "ecs_secrets" {
-  name        = "ECSAccessToSecrets"
+  name        = "ECSAccessToSecrets_snowflake"
   description = "Grant ECS tasks access to secrets"
 
   policy = jsonencode({
@@ -51,7 +51,7 @@ resource "aws_iam_policy_attachment" "ecs_secrets_attachment" {
 
 
 resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
+  name = "ecs_task_role_snowflake"
 
   assume_role_policy = <<EOF
 {
@@ -68,57 +68,4 @@ resource "aws_iam_role" "ecs_task_role" {
   ]
 }
 EOF
-}
-
-
-resource "aws_iam_policy" "full-secrets-access" {
-  name = "full-secrets-access"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Effect": "Allow",
-        "Action": [
-            "ssm:StartSession",
-            "ssm:DescribeInstanceInformation",
-            "sts:GetCallerIdentity"
-        ],
-        "Resource": "*"
-    },
-    {
-        "Effect": "Allow",
-        "Action": [
-            "ecs:ExecuteCommand",
-            "ecs:ListClusters",
-            "ecs:DescribeClusters",
-            "ecs:ListServices",
-            "ecs:DescribeServices",
-            "ecs:DescribeTasks",
-            "ecs:ListTasks"
-        ],
-        "Resource": "*"
-    },
-    {
-        "Effect": "Allow",
-        "Action": [
-            "ec2:DescribeRegions",
-            "ec2:DescribeInstances"
-        ],
-        "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-
-resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment-sidecar-destroyer" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.full-secrets-access.arn
-
-  depends_on = [
-    aws_iam_role.ecs_task_role
-  ]
 }
