d1ae4351dab540037393022e3a3758d84d5d6b21
Farid <faridwajdi05@gmail.com>
2023-11-06 22:55:33 +0700

    Add termination protection test

---
+++ tests/terraform_with_termination_protection/formal_sidecar/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    formal = {
+      version = "~> 1.0.0"
+      source  = "joinformal.com/local/formal"
+    }
+  }
+}
+
+provider "formal" {}
+
+variable "termination_protection" {
+  type        = bool
+  description = "Whether termination protection is enabled for the resource."
+}
+
+resource "formal_sidecar" "name" {
+  deployment_type        = "onprem"
+  global_kms_decrypt     = false
+  name                   = "terraform-test-local-formal-sidecar-with-termination-protection"
+  technology             = "postgres"
+  network_type           = "internal"
+  termination_protection = var.termination_protection
+}
