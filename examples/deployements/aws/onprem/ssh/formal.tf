62004f28214b3023d59d052ce753275e9575d1d7
Loris Wintjens <loris.wintjens@gmail.com>
2023-12-27 02:29:57 +0100

    ssh

--- examples/deployements/aws/onprem/ssh/formal.tf
+++ examples/deployements/aws/onprem/ssh/formal.tf
@@ -24,5 +24,28 @@ resource "formal_sidecar" "main" {
   name               = var.name
   deployment_type    = "onprem"
   technology         = "ssh"
+  network_type       = "internet-facing"
+  fail_open          = false
   global_kms_decrypt = false
+  formal_hostname    = var.hostname
+}
+
+resource "formal_datastore" "instance_1" {
+  technology = "ssh"
+  name       = var.name
+  hostname   = aws_instance.main.public_dns
+  port       = 22
+}
+
+resource "formal_native_role" "main_instance_1" {
+  datastore_id       = formal_datastore.instance_1.id
+  native_role_id     = aws_iam_access_key.example_access_key.id
+  native_role_secret = aws_iam_access_key.example_access_key.secret
+  use_as_default     = true // per sidecar, exactly one native role must be marked as the default.
+}
+
+resource "formal_sidecar_datastore_link" "link_1" {
+  datastore_id = formal_datastore.instance_1.id
+  sidecar_id   = formal_sidecar.main.id
+  port         = 22
 }
