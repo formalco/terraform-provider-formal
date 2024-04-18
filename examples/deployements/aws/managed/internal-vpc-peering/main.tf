a4d02ef931a9c8c2d4e9c12437aff987ee2ef020
Mokhtar Bacha <mokhtar@joinformal.com>
2024-04-18 02:48:01 +0200

    update doc (#250)

--- examples/deployements/aws/managed/internal-vpc-peering/main.tf
+++ examples/deployements/aws/managed/internal-vpc-peering/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     formal = {
       source  = "formalco/formal"
-      version = "~>3.0.23"
+      version = "~>4.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
