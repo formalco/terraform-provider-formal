03b17ceceb03ba6f4d9537943bd9a7050fefc6a7
Loris Wintjens <loris@joinformal.com>
2024-08-22 18:12:24 -0400

    add example

--- examples/deployements/aws/onprem/connector/common/iam.tf
+++ examples/deployements/aws/onprem/connector/common/iam.tf
@@ -56,7 +56,7 @@ resource "aws_iam_policy" "ecr_access" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action   = ["ecr:BatchGetImage","ecr:GetDownloadUrlForLayer","ecr:GetAuthorizationToken"],
+        Action   = ["ecr:BatchGetImage", "ecr:GetDownloadUrlForLayer", "ecr:GetAuthorizationToken"],
         Effect   = "Allow",
         Resource = "*"
       }
