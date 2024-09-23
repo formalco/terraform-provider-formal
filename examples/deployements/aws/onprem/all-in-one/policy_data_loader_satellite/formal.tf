4b46e12018fc7ef1fcf1dfff7f4e1329a35e488f
Guillaume Gelin <contact@ramnes.eu>
2024-09-23 09:33:07 +0200

    Add an example Policy Data Loader Satellite deployment (#361)

--- examples/deployements/aws/onprem/all-in-one/policy_data_loader_satellite/formal.tf
+++ examples/deployements/aws/onprem/all-in-one/policy_data_loader_satellite/formal.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    formal = {
+      source  = "formalco/formal"
+      version = "4.0.17"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "formal" {
+  api_key = var.formal_api_key
+}
+
+resource "formal_satellite" "main" {
+  name = "demo-policy-data-loader"
+}
+
 resource "formal_policy_data_loader" "zendesk_loader" {
   name            = "Load Zendesk tickets and related users"
   description     = "Use Zendesk API to fetch active tickets and their related users: submitters, requesters, assignees."
