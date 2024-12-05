56a8451139eb5fad84d10b0edc23220e5100e341
Victor Vieux <victor@joinformal.com>
2024-12-05 12:44:36 -0800

    add example

--- examples/deployments/aws/postgres/internal/postgres_proxy/formal.tf
+++ examples/deployments/aws/postgres/internal/postgres_proxy/formal.tf
@@ -26,6 +26,17 @@ resource "formal_resource" "main" {
   port       = var.main_port
 }
 
+# Extra hostname, used to access the same resource
+# for example: 
+# postgres_extra_name=reader_endpoint
+# postgres_extra_hostname=xxx.cluster-ro-xxx.xxx.rds.amazonaws.com
+# you can then access the resource `psql -h <var.postgres_sidecar_hostname> -p 5432 -d <dbname>@<var.name>@<var.postgres_extra_name>`
+resource "formal_resource_hostname" "name" {
+  resource_id = formal_resource.main.id
+  hostname    = var.postgres_extra_hostname
+  name        = var.postgres_extra_name
+}
+
 resource "formal_sidecar_resource_link" "main" {
   resource_id = formal_resource.main.id
   sidecar_id  = formal_sidecar.main.id
