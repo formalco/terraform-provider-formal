1de622cdf5ddc81cd612636836fe07a6beba6951
Aditya Saligrama <aditya@joinformal.com>
2025-05-01 15:54:50 -0700

    TEAMENG-221: path-scoped discovery schedules (#502)
    ## Provider
    
    ### New
    - Introduced a new optional parameter `path` for the inventory object in
    the Data Discovery resource.
    
    ### Fixed
    - No bugs or issues were resolved in this PR.
    
    ### Changed
    - Updated the Data Discovery resource to include the `path` parameter in
    both creation and update functions.
    - Adjusted the documentation to reflect the addition of the `path`
    parameter.
    
    ## Additional Information
    This change enhances the Data Discovery resource by allowing users to
    specify a path for the inventory object, thereby improving the
    flexibility and usability of the resource. The updates are reflected in
    both the codebase and the documentation to ensure clarity for future
    implementations.

--- formal/resources/resource_data_discovery.go
+++ formal/resources/resource_data_discovery.go
@@ -90,6 +90,13 @@ func ResourceDataDiscovery() *schema.Resource {
 					"mark_for_deletion",
 				}, false),
 			},
+			"path": {
+				// This description is used by the documentation generator and the language server.
+				Description: "Path of the inventory object.",
+				Type:        schema.TypeString,
+				Required:    false,
+				Optional:    true,
+			},
 		},
 	}
 }
@@ -105,12 +112,14 @@ func resourceDataDiscoveryCreate(ctx context.Context, d *schema.ResourceData, me
 	NativeUserId := d.Get("native_user_id").(string)
 	Schedule := d.Get("schedule").(string)
 	DeletionPolicy := d.Get("deletion_policy").(string)
+	Path := d.Get("path").(string)
 
 	res, err := c.Grpc.Sdk.ResourceServiceClient.CreateDataDiscoveryConfiguration(ctx, connect.NewRequest(&corev1.CreateDataDiscoveryConfigurationRequest{
 		ResourceId:     ResourceId,
 		NativeUserId:   NativeUserId,
 		Schedule:       Schedule,
 		DeletionPolicy: DeletionPolicy,
+		Path:           &Path,
 	}))
 	if err != nil {
 		return diag.FromErr(err)
@@ -150,7 +159,7 @@ func resourceDataDiscoveryUpdate(ctx context.Context, d *schema.ResourceData, me
 
 	// Only enable updates to these fields, err otherwise
 
-	fieldsThatCanChange := []string{"native_user_id", "schedule", "deletion_policy"}
+	fieldsThatCanChange := []string{"native_user_id", "schedule", "deletion_policy", "path"}
 	if d.HasChangesExcept(fieldsThatCanChange...) {
 		err := fmt.Sprintf("At the moment you can only update the following fields: %s. If you'd like to update other fields, please message the Formal team and we're happy to help.", strings.Join(fieldsThatCanChange, ", "))
 		return diag.Errorf(err)
@@ -159,8 +168,15 @@ func resourceDataDiscoveryUpdate(ctx context.Context, d *schema.ResourceData, me
 	nativeUserId := d.Get("native_user_id").(string)
 	schedule := d.Get("schedule").(string)
 	deletionPolicy := d.Get("deletion_policy").(string)
+	path := d.Get("path").(string)
 
-	_, err := c.Grpc.Sdk.ResourceServiceClient.UpdateDataDiscoveryConfiguration(ctx, connect.NewRequest(&corev1.UpdateDataDiscoveryConfigurationRequest{Id: dataDiscoveryId, NativeUserId: nativeUserId, Schedule: schedule, DeletionPolicy: deletionPolicy}))
+	_, err := c.Grpc.Sdk.ResourceServiceClient.UpdateDataDiscoveryConfiguration(ctx, connect.NewRequest(&corev1.UpdateDataDiscoveryConfigurationRequest{
+		Id:             dataDiscoveryId,
+		NativeUserId:   nativeUserId,
+		Schedule:       schedule,
+		DeletionPolicy: deletionPolicy,
+		Path:           &path,
+	}))
 	if err != nil {
 		return diag.FromErr(err)
 	}
