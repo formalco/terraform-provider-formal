7b783aff54afe2ae859e669ba5e785ce08768ae0
ramnes <contact@ramnes.eu>
2025-07-23 00:05:43 +0200

    Fix outputs

--- examples/deployments/aws/connector/connector/outputs.tf
+++ examples/deployments/aws/connector/connector/outputs.tf
@@ -2,8 +2,3 @@ output "connector_id" {
   description = "The ID of the Formal connector"
   value       = formal_connector.main.id
 }
-
-output "nlb_dns_name" {
-  description = "DNS name of the connector's Network Load Balancer"
-  value       = aws_lb.main.dns_name
-}
---
+++ examples/deployments/aws/connector/outputs.tf
@@ -0,0 +1,4 @@
+output "connector_hostname" {
+  description = "Generated Formal Connector hostname (with automatic TLS)"
+  value       = local.connector_hostname
+}
