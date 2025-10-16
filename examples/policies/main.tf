228f3481e8a052b74f3bca0dc2852c8e9126af63
dhylan01 <77525379+dhylan01@users.noreply.github.com>
2025-10-15 20:06:54 -0700

    Fix Policy Resource Examples (#614)
    - created,updated,and deleted all resources in the policies folder
    - NOTE: the following files seem like they have no use and we should
    remove:
    examples/policies/environment.tfvars
    examples/policies/variables.tf

--- examples/policies/main.tf
+++ examples/policies/main.tf
@@ -19,10 +19,9 @@ provider "formal" {
 resource "formal_policy" "decrypt" {
   name         = "decrypt"
   description  = "decrpyt any columns that has the name encrypted-col."
-  owners       = ["john@formal.com"]
+  owner        = "john@company.com"
   notification = "none"
   status       = "active"
-  active       = true
   module       = <<-EOF
 package formal.v2
 
@@ -39,17 +38,16 @@ EOF
 resource "formal_policy" "mask_emails" {
   name         = "mask-email"
   description  = "Mask any column that has the data label email_address."
-  owners       = ["john@company.com"]
+  owner        = "john@company.com"
   notification = "consumer"
   status       = "active"
-  active       = true
   module       = <<-EOF
 package formal.v2
 
 import future.keywords.if
 
 post_request := { "action": "mask", "type": "redact.partial", "sub_type": "email_mask_username", "columns": columns, "typesafe": "fallback_to_default" } if {
-    columns := [col | col := input.columns[_]; col["data_label"] == "email_address";]
+    columns := [col | col := input.columns[_]; col["data_label"] == "email_address"]
 }
 EOF
 }
@@ -57,10 +55,9 @@ EOF
 resource "formal_policy" "row_level_hashing" {
   name         = "test-row-level-hashing-eu"
   description  = "hash every row that has the eu column set to true."
-  owners       = ["john@company.com"]
+  owner        = "john@company.com"
   notification = "all"
   status       = "active"
-  active       = true
   module       = <<-EOF
 package formal.v2
 
@@ -76,9 +73,8 @@ EOF
 resource "formal_policy" "block_db_with_formal_message" {
   name         = "block_db_with_formal_message"
   description  = "this policy block connection to sidecar based on the name of db and drop the connection with a formal message"
-  owners       = ["john@company.com"]
+  owner        = "john@company.com"
   notification = "all"
-  active       = true
   status       = "active"
   module       = <<-EOF
 package formal.v2
@@ -100,10 +96,9 @@ EOF
 resource "formal_policy" "http_pre_request_name_hash" {
   name         = "http_pre_request_name_hash"
   description  = "this policy hash every names in body request of HTTP requests"
-  owners       = ["john@company.com"]
+  owner        = "john@company.com"
   notification = "all"
   status       = "active"
-  active       = true
   module       = <<-EOF
 package formal.v2
 
